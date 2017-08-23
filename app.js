a0bda1f48c6e3dab287057d43c4ff358d2fb60ba
KnightNiwrem <merwinwee@gmail.com>
2017-08-23 16:44:31 +0800

    Update with safe Database credentials (blocked from public access), removed createdb api

--- app.js
+++ app.js
@@ -3,33 +3,27 @@ const mysql = require('mysql');
 
 // create connection
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password: 'limtaeu'
+    host : 'nusreviews-db.cwdwdifwzxfv.ap-southeast-1.rds.amazonaws.com',
+    user : 'nusreviews',
+    password: '1qw23er4'
 });
 
 // connect
 db.connect((err) => {
     if(err){
-        //throw err;
+        throw err;
+    } else {
+        console.log('MySQL Connected');
     }
-    console.log('My sql Connected');
 });
 
 const app = express();
 
-// Create db
-app.get('/createdb', (req, res) =>{
-    let sql = 'CREATE DATABASE testsql';
-    db.query(sql, (err, result)=>{
-        if(err){
-            //throw err;
-        } 
-        console.log(result);
-        res.send("database created...");
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World"
     });
 });
-
 
 app.listen('3000', ()=>{
     console.log('Server started on port 3000');
