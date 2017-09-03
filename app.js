40bb73639878bd53f8797375c43108d8994c7a9b
KnightNiwrem <merwinwee@gmail.com>
2017-09-03 21:39:09 +0800

    Test console

--- app.js
+++ app.js
@@ -112,6 +112,7 @@ app.get("/generateServerToken", (req, res) => {
 
     exchangeFbToken(fbToken).then((fbResponse) => {
         let newFbToken = fbResponse.access_token;
+        console.log(fbResponse);
         console.log(newFbToken);
 
         User.findOrCreate({
