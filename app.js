b5f6c47f1beb30897d3f6bbd7c5815c0bef3ab02
KnightNiwrem <merwinwee@gmail.com>
2017-09-03 18:13:54 +0800

    Add allow headers to headers

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
